d697b1d8ffa3ae8b1541946f359120302d492fbc
proninjacode07@gmail.com <proninjacode07@gmail.com>
2022-12-17 08:50:19 +0530

    feat: login design improv

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:automation_wrapper_builder/core/utils/app_utils.dart';
 import 'package:automation_wrapper_builder/core/utils/ui_helper.dart';
+import 'package:automation_wrapper_builder/core/widgets/app_padding.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -24,39 +26,57 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
+      body: DefaultAppPadding(
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _emailCtrl,
-                validator: AppUtils.emailValidate,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: ResponsiveSizingConfig.instance.breakpoints.tablet,
               ),
-              verticalSpaceRegular,
-              TextFormField(
-                controller: _passCtrl,
-                validator: AppUtils.passwordValidate,
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome back",
+                    style: textTheme(context).headlineMedium!.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                  verticalSpaceSmall,
+                  Text(
+                    "Please login to continue",
+                    style: textTheme(context).bodyLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  verticalSpaceMedium,
+                  TextFormField(
+                    controller: _emailCtrl,
+                    validator: AppUtils.emailValidate,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  verticalSpaceRegular,
+                  TextFormField(
+                    controller: _passCtrl,
+                    validator: AppUtils.passwordValidate,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  verticalSpaceRegular,
+                  ElevatedButton.icon(
+                    icon: const Icon(Icons.login),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    label: const Text('Sign In'),
+                  ),
+                ],
               ),
-              verticalSpaceRegular,
-              ElevatedButton.icon(
-                icon: const Icon(Icons.login),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {}
-                },
-                label: const Text('Sign In'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
