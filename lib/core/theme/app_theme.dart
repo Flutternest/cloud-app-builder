a2a77363192b95fcb9bcb1d09356a7ff525b1cc8
proninjacode07@gmail.com <proninjacode07@gmail.com>
2022-12-17 08:45:03 +0530

    feat: add login page

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,10 +4,20 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final lightTheme = ThemeData.light().copyWith(
     brightness: Brightness.light,
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15)),
+    ),
     colorScheme: lightColorScheme,
   );
   static final darkTheme = ThemeData.dark().copyWith(
     brightness: Brightness.dark,
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(),
+    ),
     colorScheme: lightColorScheme,
   );
 }
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,7 +1,25 @@
+import 'package:automation_wrapper_builder/core/utils/app_utils.dart';
+import 'package:automation_wrapper_builder/core/utils/ui_helper.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailCtrl = TextEditingController();
+  final _passCtrl = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailCtrl.dispose();
+    _passCtrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +27,38 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: const Center(
-        child: Text('Login Page'),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailCtrl,
+                validator: AppUtils.emailValidate,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              verticalSpaceRegular,
+              TextFormField(
+                controller: _passCtrl,
+                validator: AppUtils.passwordValidate,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              verticalSpaceRegular,
+              ElevatedButton.icon(
+                icon: const Icon(Icons.login),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+                label: const Text('Sign In'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
