2909b5d8fd8349b8c9598f9bb3ab5ae7b11acfbf
proninjacode07@gmail.com <proninjacode07@gmail.com>
2022-12-17 11:54:56 +0530

    feat: add hover effect to info containers

--- lib/views/widgets/info_container_row.dart
+++ lib/views/widgets/info_container_row.dart
@@ -1,6 +1,8 @@
 import 'package:automation_wrapper_builder/core/utils/ui_helper.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/widgets/hover_builder.dart';
+
 class InfoContainerRow extends StatelessWidget {
   const InfoContainerRow({super.key});
 
@@ -26,42 +28,44 @@ class InfoContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / 10,
-      height: MediaQuery.of(context).size.width / 10,
-      // padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: theme(context).colorScheme.primary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: theme(context).colorScheme.inversePrimary,
-              padding: const EdgeInsets.all(8),
-              child: Center(
-                child: Text(
-                  value,
-                  style: textTheme(context).displaySmall!.copyWith(
-                        color: theme(context).colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                      ),
+    return HoverEffect(
+      child: Container(
+        width: MediaQuery.of(context).size.width / 10,
+        height: MediaQuery.of(context).size.width / 10,
+        // padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: theme(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: theme(context).colorScheme.inversePrimary,
+                padding: const EdgeInsets.all(8),
+                child: Center(
+                  child: Text(
+                    value,
+                    style: textTheme(context).displaySmall!.copyWith(
+                          color: theme(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              contentType,
-              style: textTheme(context).titleSmall!.copyWith(
-                    color: theme(context).colorScheme.onPrimary,
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                contentType,
+                style: textTheme(context).titleSmall!.copyWith(
+                      color: theme(context).colorScheme.onPrimary,
+                    ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
