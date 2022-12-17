0ec9a3f543a6ca36de85a2abe799fe6004ef8e80
proninjacode07@gmail.com <proninjacode07@gmail.com>
2022-12-17 16:31:53 +0530

    feat: add more fields & loader implementation

--- lib/views/add_app_page.dart
+++ lib/views/add_app_page.dart
@@ -1,6 +1,9 @@
+import 'package:automation_wrapper_builder/controllers/selected_menu_controller.dart';
 import 'package:automation_wrapper_builder/core/utils/app_utils.dart';
 import 'package:automation_wrapper_builder/core/utils/ui_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../core/widgets/app_padding.dart';
 
@@ -59,6 +62,8 @@ class AddAppPage extends StatelessWidget {
               ),
             ),
             verticalSpaceRegular,
+            const Center(child: Text("Resolution: 514x514 (1:1 aspect ratio)")),
+            verticalSpaceRegular,
             const AddAppForm(),
           ],
         ),
@@ -67,134 +72,234 @@ class AddAppPage extends StatelessWidget {
   }
 }
 
-class AddAppForm extends StatefulWidget {
+class AddAppForm extends ConsumerStatefulWidget {
   const AddAppForm({super.key});
 
   @override
-  State<AddAppForm> createState() => _AddAppFormState();
+  ConsumerState<AddAppForm> createState() => _AddAppFormState();
 }
 
-class _AddAppFormState extends State<AddAppForm> {
+class _AddAppFormState extends ConsumerState<AddAppForm> {
   final _formKey = GlobalKey<FormState>();
   final _appNameController = TextEditingController();
   final _bundleIdController = TextEditingController();
   final _colorController = TextEditingController();
   final _websiteUrlController = TextEditingController();
+  final _versionNumberController = TextEditingController();
+  final _versionController = TextEditingController();
+
   final _toEmailController =
       TextEditingController(text: "admin@awabuilder.com");
 
+  bool isLoading = false;
+
+  @override
+  void dispose() {
+    //dispose all controllers
+    _appNameController.dispose();
+    _bundleIdController.dispose();
+    _colorController.dispose();
+    _websiteUrlController.dispose();
+    _versionNumberController.dispose();
+    _versionController.dispose();
+    _toEmailController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            controller: _appNameController,
-            validator: AppUtils.fieldEmpty,
-            decoration: const InputDecoration(
-              labelText: "App Name",
-              hintText: "Enter App Name",
+    return FocusTraversalGroup(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _appNameController,
+              validator: AppUtils.fieldEmpty,
+              decoration: const InputDecoration(
+                labelText: "App Name",
+                hintText: "Enter App Name",
+              ),
             ),
-          ),
-          verticalSpaceRegular,
-          TextFormField(
-            controller: _bundleIdController,
-            validator: (value) {
-              if (value == null) {
-                return "Please enter a valid app bundle identifier";
-              }
-              if (value.isEmpty) {
-                return 'Please enter a valid app bundle identifier';
-              }
-
-              return null;
-            },
-            decoration: const InputDecoration(
-              labelText: "Bundle ID",
-              hintText: "Ex: com.betpredictions.app",
-            ),
-          ),
-          verticalSpaceRegular,
-          Tooltip(
-            message:
-                "Do not include the Hash (#) symbol while entering the color code",
-            child: TextFormField(
-              controller: _colorController,
+            verticalSpaceRegular,
+            TextFormField(
+              controller: _bundleIdController,
               validator: (value) {
-                if (value == null) return "Please enter a valid hex color code";
-                if (value.isEmpty) {
-                  return 'Please enter a valid hex color code';
+                if (value == null) {
+                  return "Please enter a valid app bundle identifier";
                 }
-                // Check that the value is a valid hex color code (e.g. FF0000)
-                if (!RegExp(r'^[0-9A-Fa-f]{6}$').hasMatch(value)) {
-                  return 'Invalid hex color code';
+                if (value.isEmpty) {
+                  return 'Please enter a valid app bundle identifier';
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: "Bundle ID",
+                hintText: "Ex: com.betpredictions.app",
+              ),
+            ),
+            verticalSpaceRegular,
+            Tooltip(
+              message:
+                  "Do not include the Hash (#) symbol while entering the color code",
+              child: TextFormField(
+                controller: _colorController,
+                validator: (value) {
+                  if (value == null) {
+                    return "Please enter a valid hex color code";
+                  }
+                  if (value.isEmpty) {
+                    return 'Please enter a valid hex color code';
+                  }
+                  // Check that the value is a valid hex color code (e.g. FF0000)
+                  if (!RegExp(r'^[0-9A-Fa-f]{6}$').hasMatch(value)) {
+                    return 'Invalid hex color code';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: "Color (HEX)",
+                  hintText: "Ex: FFF000",
+                ),
+              ),
+            ),
+            verticalSpaceRegular,
+            TextFormField(
+              controller: _websiteUrlController,
+              validator: (value) {
+                if (value == null) return "Please enter a valid website URL";
+                if (value.isEmpty) {
+                  return 'Please enter a valid website URL';
+                }
+                // Check that the value is a valid website URL
+                if (!RegExp(
+                        r'^(https?:\/\/){1}([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$')
+                    .hasMatch(value)) {
+                  return 'Invalid website URL';
                 }
                 return null;
               },
               decoration: const InputDecoration(
-                labelText: "Color (HEX)",
-                hintText: "Ex: FFF000",
+                labelText: "Website URL",
+                hintText: "Ex: https://betpredictions.app/user_abc",
               ),
             ),
-          ),
-          verticalSpaceRegular,
-          TextFormField(
-            controller: _websiteUrlController,
-            validator: (value) {
-              if (value == null) return "Please enter a valid website URL";
-              if (value.isEmpty) {
-                return 'Please enter a valid website URL';
-              }
-              // Check that the value is a valid website URL
-              if (!RegExp(
-                      r'^(https?:\/\/){1}([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$')
-                  .hasMatch(value)) {
-                return 'Invalid website URL';
-              }
-              return null;
-            },
-            decoration: const InputDecoration(
-              labelText: "Website URL",
-              hintText: "Ex: https://betpredictions.app/user_abc",
+            verticalSpaceRegular,
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _versionController,
+                    validator: (value) {
+                      if (value == null) {
+                        return "Please enter a valid version code (ex: 1.0.0)";
+                      }
+
+                      if (value.isEmpty) {
+                        return 'Please enter a version code (ex: 1.0.0)';
+                      }
+                      // Check the format of the input.
+                      // Android version codes are typically in the form 'x.y.z', where x, y, and z are integers.
+                      // iOS version codes are typically in the form 'x.y.z', where x, y, and z are integers.
+                      if (!RegExp(r'^\d+\.\d+\.\d+$').hasMatch(value)) {
+                        return 'Please enter a valid version code (ex: 1.0.0)';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Version Code",
+                      hintText: "Ex: 1.0.0",
+                    ),
+                  ),
+                ),
+                horizontalSpaceRegular,
+                Expanded(
+                  child: TextFormField(
+                    controller: _versionNumberController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    validator: (value) {
+                      if (value == null) {
+                        return "Please enter a valid version/build number";
+                      }
+                      if (value.isEmpty) {
+                        return 'Please enter a valid version/build number';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Version/Build Number",
+                      hintText: "Ex: 12",
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          verticalSpaceRegular,
-          TextFormField(
-            controller: _toEmailController,
-            validator: AppUtils.emailValidate,
-            decoration: const InputDecoration(
-              labelText: "Email address to send",
-              hintText: "admin@betpredictions.com",
+            verticalSpaceRegular,
+            TextFormField(
+              controller: _toEmailController,
+              validator: AppUtils.emailValidate,
+              decoration: const InputDecoration(
+                labelText: "Email address to send",
+                hintText: "admin@betpredictions.com",
+              ),
             ),
-          ),
-          verticalSpaceMedium,
-          ElevatedButton.icon(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                ScaffoldMessenger.of(context).showMaterialBanner(
-                  MaterialBanner(
-                    leading: const Icon(Icons.check_circle_outline),
-                    backgroundColor: Colors.greenAccent,
-                    content: const Text(
-                        "App added successfully. You'll get an email when it's ready"),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
+            verticalSpaceMedium,
+            ElevatedButton.icon(
+              icon: isLoading
+                  ? const SizedBox(
+                      height: 20, width: 20, child: CircularProgressIndicator())
+                  : const Icon(Icons.add),
+              label: Text(isLoading ? "Creating..." : "Create App"),
+              onPressed: isLoading
+                  ? null
+                  : () async {
+                      if (_formKey.currentState!.validate()) {
+                        final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+                        setState(() => isLoading = true);
+                        await Future.delayed(const Duration(seconds: 1))
+                            .then((value) {
+                          setState(() => isLoading = false);
+
+                          _appNameController.clear();
+                          _bundleIdController.clear();
+                          _colorController.clear();
+                          _websiteUrlController.clear();
+                          _versionController.clear();
+                          _versionNumberController.clear();
+                        });
+
+                        scaffoldMessenger.showMaterialBanner(
+                          MaterialBanner(
+                            leading: const Icon(Icons.check_circle_outline),
+                            backgroundColor: Colors.greenAccent,
+                            content: const Text(
+                                "App added successfully. You'll get an email when it's ready. Redirecting to Dashboard..."),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentMaterialBanner();
+                                },
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          ),
+                        );
+                        Future.delayed(const Duration(seconds: 5), () {
                           ScaffoldMessenger.of(context)
                               .hideCurrentMaterialBanner();
-                        },
-                        child: const Text("OK"),
-                      )
-                    ],
-                  ),
-                );
-              }
-            },
-            label: const Text("Create App"),
-          ),
-        ],
+                          ref.read(selectedMenuProvider.notifier).state =
+                              SidebarMenuItem.dashboard;
+                        });
+                      }
+                    },
+            ),
+          ],
+        ),
       ),
     );
   }
